a40737f7f57e240687c7c8ccdbb7a13dac1db43b
BoRayan <Alimohammad-s@hotmail.com>
2017-04-19 16:39:17 +0300

    //

--- Assignment 3/multiagent/multiAgents.py
+++ Assignment 3/multiagent/multiAgents.py
@@ -346,7 +346,7 @@ def betterEvaluationFunction(currentGameState):
     else:
         closestGhostDistanceScared = 0
 
-    score = currentScore + (-10 * closestFoodPellet) + (-5 * closestGhostDistanceScared) \
+    score = currentScore + (-1 * closestFoodPellet) + (-3 * closestGhostDistanceScared) \
             + (-80 * powerFoodPellets) + (-4 * foodPellets)
 
     return score
